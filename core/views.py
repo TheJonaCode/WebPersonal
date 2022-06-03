a46abf21a6fd0cae2a413d4a39254750fd78fea7
Jonathan <mcjhona069@gmail.com>
2022-06-03 18:55:02 -0500

    Templates about, portfolio y contact2

--- core/views.py
+++ core/views.py
@@ -1,31 +1,13 @@
-from django.shortcuts import render, HttpResponse
+    from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-html_base = """
-    <h1>Mi Web Personal</h1>
-    <ul>
-        <li><a href="/">Portada</a></li>
-        <li><a href="/about/">Acerca de</a></li>
-        <li><a href="/contact/">Contacto</a></li>
-    </ul>
-"""
+    def home(request):
+        return render(request, "core/home.html")
 
-def home(request):
-    return render(request, "core/home.html")
+    def about(request):
+        return render(request, "core/about.html")
 
-def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Me llamo Jonathan y me agrada Django!</p>
-    """)
+    def portfolio(request):
+        return render(request, "core/portfolio.html")
 
-def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aquí les dejo mi email y mis redes sociales:</p>
-        <ul>
-            <li><a href="mailto:hola@hektorprofe.net">Email</a></li>
-            <li><a href="https://github.com/thejonacode">Github</a></li>
-            <li><a href="https://youtube.com">Youtube</a></li>
-        </ul>
-    """)
+    def contact(request):
+        return render(request, "core/contact.html")
